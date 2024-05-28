cfa08b9d75ad842f9a74b55c4ae1aa7164a7d1f5
Eden Azoulay <edenaz31@gmail.com>
2024-05-28 12:23:12 +0300

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,63 @@
 // NAV BAR
+
 document.getElementById("form").addEventListener("submit", function (e) {
-    e.preventDefault(); // Prevent the default form submission
-    document.getElementById("message").textContent = "Submitting..";
-    document.getElementById("message").style.display = "block";
-    document.getElementById("submit-button").disabled = true;
+  e.preventDefault(); // Prevent the default form submission
+  document.getElementById("message").textContent = "Submitting..";
+  document.getElementById("message").style.display = "block";
+  document.getElementById("submit-button").disabled = true;
 
-    // Collect the form data
-    var formData = new FormData(this);
-    var keyValuePairs = [];
-    for (var pair of formData.entries()) {
-      keyValuePairs.push(pair[0] + "=" + pair[1]);
+  // Collect the form data
+  var formData = new FormData(this);
+  var keyValuePairs = [];
+  for (var pair of formData.entries()) {
+    keyValuePairs.push(pair[0] + "=" + pair[1]);
+  }
+
+  var formDataString = keyValuePairs.join("&");
+
+  // Send a POST request to your Google Apps Script
+  fetch(
+    "https://script.google.com/macros/s/AKfycbxLGz4TPux4a2klB9swU_zTfRtu1RypM2g6c7V0tXuk8KiPmsfpFLH-grxWGA2odEClIQ/exec",
+    {
+      redirect: "follow",
+      method: "POST",
+      body: formDataString,
+      headers: {
+        "Content-Type": "text/plain;charset=utf-8",
+      },
     }
-
-    var formDataString = keyValuePairs.join("&");
-
-    // Send a POST request to your Google Apps Script
-    fetch(
-      "https://script.google.com/macros/s/AKfycbykFFtGW0fSshR2wOQTUV8Mn5W5tvFe4JYkVarOV8xiLX6BVrRNJstJDPqgZPlkQADwCg/exec",
-      {
-        redirect: "follow",
-        method: "POST",
-        body: formDataString,
-        headers: {
-          "Content-Type": "text/plain;charset=utf-8",
-        },
+  )
+    .then(function (response) {
+      // Check if the request was successful
+      if (response) {
+        return response; // Assuming your script returns JSON response
+      } else {
+        throw new Error("Failed to submit the form.");
       }
-    )
-      .then(function (response) {
-        // Check if the request was successful
-        if (response) {
-          return response; // Assuming your script returns JSON response
-        } else {
-          throw new Error("Failed to submit the form.");
-        }
-      })
-      .then(function (data) {
-        // Display a success message
-        document.getElementById("message").textContent =
-          "Data submitted successfully!";
-        document.getElementById("message").style.display = "block";
-        document.getElementById("message").style.backgroundColor = "green";
-        document.getElementById("message").style.color = "beige";
-        document.getElementById("submit-button").disabled = false;
-        document.getElementById("form").reset();
+    })
+    .then(function (data) {
+      // Display a success message
+      document.getElementById("message").textContent =
+        "Data submitted successfully!";
+      document.getElementById("message").style.display = "block";
+      document.getElementById("message").style.backgroundColor = "green";
+      document.getElementById("message").style.color = "beige";
+      document.getElementById("submit-button").disabled = false;
+      document.getElementById("form").reset();
 
-        setTimeout(function () {
-          document.getElementById("message").textContent = "";
-          document.getElementById("message").style.display = "none";
-        }, 2600);
-      })
-      .catch(function (error) {
-        // Handle errors, you can display an error message here
-        console.error(error);
-        document.getElementById("message").textContent =
-          "An error occurred while submitting the form.";
-        document.getElementById("message").style.display = "block";
-      });
-  });
+      setTimeout(function () {
+        document.getElementById("message").textContent = "";
+        document.getElementById("message").style.display = "none";
+      }, 2600);
+    })
+    .catch(function (error) {
+      // Handle errors, you can display an error message here
+      console.error(error);
+      document.getElementById("message").textContent =
+        "An error occurred while submitting the form.";
+      document.getElementById("message").style.display = "block";
+    });
+});
   // MENU MOBILE
   
   document.addEventListener('DOMContentLoaded', () => {
